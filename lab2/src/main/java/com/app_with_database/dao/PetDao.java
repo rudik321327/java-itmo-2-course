0fe21eb267baf8918bc773fc3c190d0883f395d7
rudik321327 <bcoin3213276577565@gmail.com>
2026-05-23 14:53:25 +0300

    Delete lab2 directory

--- lab2/src/main/java/com/app_with_database/dao/PetDao.java
+++
@@ -1,3 +0,0 @@
-package com.app_with_database.dao;
-import com.app_with_database.model.Pet;
-public class PetDao extends GenericDaoJPA<Pet> { }
--- lab2/src/main/java/com/app_with_database/model/Color.java
+++
@@ -1,5 +0,0 @@
-package com.app_with_database.model;
-
-public enum Color {
-    BLACK, WHITE, BROWN, GRAY, ORANGE
-}
